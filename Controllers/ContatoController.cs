d03c3a827a654b9a310547821eb808f52b903078
KayoLeanndro <kayoleanndro2@gmail.com>
2024-02-05 18:37:06 -0300

    Making Contact_HTML & ContextController

---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProjetoMVC.Context;
+using ProjetoMVC.Models;
+
+namespace ProjetoMVC.Controllers
+{
+    public class ContatoController : Controller
+    {
+        private readonly AgendaContext _context;
+
+        public ContatoController(AgendaContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var contatos = _context.contatos.ToList();
+            return View(contatos);
+        }
+    }
+}
